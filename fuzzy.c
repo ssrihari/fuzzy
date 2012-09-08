2a73e81e2e61370dbfacab3e9815b1322dbbf1c0
Srihari Sriraman <srihari@c42.in>
2012-09-08 23:33:22 +0530

    Revert "Renamed function. Refactored return."
    This reverts commit adc249127eca3b484627e025c960227739cc1000.

--- fuzzy.c
+++ fuzzy.c
@@ -2,26 +2,27 @@
 #include <stdbool.h>
 #include <string.h>
 
-bool fuzzy_search(char *key, char *string)
+bool fuzzy(char *key, char *string)
 {
   char *remaining_string = string;
   if ( strlen(key) == 0 || strlen(remaining_string) == 0 )
       return true;
 
   if ( (remaining_string = strchr(string, key[0])) != NULL )
-      fuzzy_search( key+1, remaining_string+1 );
-  return false;
+      fuzzy( key+1, remaining_string+1 );
+  else
+    return false;
 }
 
 main()
 {
-  char string[200] = "hollo i am srihari and i am trying out fuzzy_search search for tito";
+  char string[200] = "hollo i am srihari and i am trying out fuzzy search for tito";
 
   char *key;
   bool status;
 
   printf("%s", "Enter something:");
   scanf("%s", key);
-  status = fuzzy_search(key, string);
-  printf("The fuzzy_search string match returned %d\n", status);
+  status = fuzzy(key, string);
+  printf("The fuzzy string match returned %d\n", status);
 }
